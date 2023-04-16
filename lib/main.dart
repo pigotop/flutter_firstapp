9a64b132489130491d27613d38bf0b6c5360680d
MSI\pigot <puvanatwic@gosoft.co.th>
2023-04-16 14:58:32 +0700

    เพิ่ม widget  ใหม่

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,42 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Demo Test Flutter',
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Demo'),
+          ),
+          body: Column(
+            children: [titleSection],
+          )),
     );
   }
 }
+
+Widget titleSection = Container(
+    padding: const EdgeInsets.all(32),
+    child: Row(
+      children: [
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: const Text('Oeschinen Lake Campground',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+            ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(color: Colors.grey[500]),
+            )
+          ],
+        )),
+        Icon(
+          Icons.star,
+          color: Colors.red[500],
+        ),
+        const Text('41')
+      ],
+    ));
