c1cf2dc945c52aa050fa1b6bae8271ab691d6144
MSI\pigot <puvanatwic@gosoft.co.th>
2023-04-16 16:53:33 +0700

    สร้าง icon ที่รองรับการเรียกใช้

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE')
+      ],
+    );
+
     return MaterialApp(
       title: 'Demo Test Flutter',
       home: Scaffold(
@@ -16,10 +27,31 @@ class MainApp extends StatelessWidget {
             title: const Text('Demo'),
           ),
           body: Column(
-            children: [titleSection],
+            children: [titleSection, buttonSection],
           )),
     );
   }
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(
+        icon,
+        color: color,
+      ),
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+              fontSize: 12, fontWeight: FontWeight.w400, color: color),
+        ),
+      )
+    ],
+  );
 }
 
 Widget titleSection = Container(
