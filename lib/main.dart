cf8a54d94cb02a2d959c003ca878b3fe468f307e
MSI\pigot <puvanatwic@gosoft.co.th>
2023-04-16 22:27:18 +0700

    เพิ่ม image  และปรับ  column เป็น Listview

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,18 @@ class MainApp extends StatelessWidget {
           appBar: AppBar(
             title: const Text('Demo'),
           ),
-          body: Column(
-            children: [titleSection, buttonSection],
+          body: ListView(
+            children: [
+              Image.asset(
+                'images/lake.jpg',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textSection
+            ],
           )),
     );
   }
@@ -80,3 +90,16 @@ Widget titleSection = Container(
         const Text('41')
       ],
     ));
+
+Widget textSection = const Padding(
+  padding: EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
